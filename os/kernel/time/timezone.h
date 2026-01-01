0d5e6163cc8333a763bbe81c3ef773a20e09fbd0
mihai209 <sbarceamihai222@gmail.com>
2026-01-01 17:46:06 +0200

    feat

--- os/kernel/time/timezone.h
+++ os/kernel/time/timezone.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define TZ_UTC   0
-#define TZ_RO    2   // România (fără DST)
+#define TZ_RO    2   // România (without DST)
 #define TZ_UK    0
 #define TZ_DE    1
 #define TZ_FR    1
